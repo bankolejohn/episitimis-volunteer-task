a33e9cecc0e8bb0e40ce8bab70d3d823ff20585e
bankolejohn <bankolejohn@gmail.com>
2023-11-18 03:05:19 +0100

    update code

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ function Home() {
     <div className="home" css={CSS}>
       <div className="banner">
         <div className="headline">
-          <h1>Welcome to Devops project Bank!</h1>
+          <h1>Welcome to EPISTIMIS Devops project Bank!</h1>
           <p>Mission Critical Task...</p>
         </div>
         <div className="image">
